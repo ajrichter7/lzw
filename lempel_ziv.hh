b664729737ee9f23670013000eef1f26641cbb27
flastest <pleasantbowel5yndrome@gmail.com>
2021-10-24 14:49:26 -0700

    added the namespace

--- lempel_ziv.hh
+++ lempel_ziv.hh
@@ -4,18 +4,21 @@
 #include <vector>
 
 
+using bit_stream_t = std::vector<bool>;
 //this is an implementation for lempel ziv
 
 namespace LZW
 {
 	//this will compress a string of bools 
-	vector<bool> compress (vector<bool> input);
+	bit_stream_t compress (std::vector<bool> input);
 
 	//overloads for taking in numbers and strings
-	vector<bool> compress (string int);
-	vector<bool> compress (string input);
+	bit_stream_t compress (std::vector<int> input);
+	bit_stream_t compress (std::string input);
 
 
-
-	decompress
+	//we need decompress for bool vectors, strings, and ints
+	std::vector<bool> decompress_to_bools (bit_stream_t input);
+	std::vector<int> decompress_to_ints (bit_stream_t input);
+	std::string decompress_to_string (bit_stream_t input);
 }

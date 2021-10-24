402c4a59c3b1d1d62f8448433083efcab2f3e2f0
flastest <pleasantbowel5yndrome@gmail.com>
2021-10-24 15:19:32 -0700

    added basic implementation

--- lempel_ziv.cc
+++ lempel_ziv.cc
@@ -1,3 +1,37 @@
 #include "lempel_ziv.hh"
 
 
+//bit_stream_t = std::vector<bool>;
+
+LZW::compress(std::vector<bool> input) {
+	bit_stream_t output;
+	return output;
+}
+
+
+LZW::compress(std::vector<int> input) {
+	bit_stream_t output;
+	return output;
+}
+
+LZW::compress(std::string input) {
+	bit_stream_t output;
+	return output;
+}
+
+
+LZW::decompress_to_bools (bit_stream_t input)
+{
+	std::vector<bool> output;
+	return output;
+}
+LZW::decompress_to_ints (bit_stream_t input)
+{
+	std::vector<int> output;
+	return output;
+}
+LZW::decompress_to_string (bit_stream_t input)
+{
+	std::string output;
+	return output;
+}

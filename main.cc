667f523bc569e3414ced8807af5fb77fbae2699a
ajrichter7 <alexis.richter7@gmail.com>
2021-10-24 17:12:24 -0700

    main

--- main.cc
+++ main.cc
@@ -5,18 +5,6 @@
 #include "bitio.hh"
 
 
-void readfile(std::string textfile){
-	std::ifstream file (textfile);
-	std::cout << "die";
-	std::string bitstream;
-	if (file.is_open()) {
-		while (file.good()){
-			file >> bitstream;
-			std::cout << bitstream;
-		}
-	}
-};
-
 int main(int argc, char** argv) {
 	return 0;
 }

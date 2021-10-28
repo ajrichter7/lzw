b09d7a19c786dd0820a14fcfe30c174bcd67c417
flastest <pleasantbowel5yndrome@gmail.com>
2021-10-27 19:30:53 -0700

    adding main.cc

--- main.cc
+++ main.cc
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <ostream>
 #include <fstream>
+#include <cassert>
 #include "lempel_ziv.hh"
 #include "bitio.hh"
 
 
 int main(int argc, char** argv) {
-	return 0;
+	//get arg from user
+
+	assert(argc>1&& "moar args pl0x uwu (read: give me a file to do this on)");
+
+	std::string input_file(argv[1]);
+	std::cout<<input_file<<std::endl;
+
 }
